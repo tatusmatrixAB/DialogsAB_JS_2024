b7a98f364ca8f77051028cf89c984b723791d601
tatusmatrixAB <tatusmatrix@gmail.com>
2024-06-03 21:08:59 +0300

    Nodarbiba_04a

---
+++ Nodarbiba_04/JS/array_1.js
@@ -0,0 +1,13 @@
+console.log(new Date());
+
+/*
+const cars1 = "Saab";
+const cars2 = "Volvo";
+const cars3 = "BMW";
+document.getElementById("demo").innerHTML = cars1+','+cars2+','+cars3;
+*/
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars;
+
