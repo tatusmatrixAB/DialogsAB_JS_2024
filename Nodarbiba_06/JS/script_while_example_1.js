a2c65239f918287c8cc92177995647f5dff155db
tatusmatrixAB <tatusmatrix@gmail.com>
2024-06-10 19:48:31 +0300

    Nodarbiba_06d

--- Nodarbiba_06/JS/script_while_example_1.js
+++ Nodarbiba_06/JS/script_while_example_1.js
@@ -2,17 +2,37 @@
 
 let text = "";
 let i = 0;
+let j = 0;
 
-console.log("i = "+i);
+console.log("i = " + i);
 console.log(i < 10);
-console.log("text = "+text);
+console.log("text = " + text);
 
 while (i < 10) {
-  text += "<br>The number is " + i;  
+  text += "<br>The number is " + i;
   i++;
 
-  console.log("i = "+i);
+  j = 0;
+  while (j < 5) {
+    if (j == 2) {
+      j = j + 0.5;
+      continue;
+    }
+
+    if (j == 3) {
+      break;
+    }
+
+    j = j + 0.5;
+    console.log("j = " + j);
+  }
+
+  //if (i == 4) {continue;}
+
+  //if (i == 7) {break;}
+
+  console.log("i = " + i);
   console.log(i < 10);
-  console.log("text = "+text);
+  console.log("text = " + text);
 }
 document.getElementById("demo").innerHTML = text;
