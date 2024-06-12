cb5319123331e9d605bb38e1f8a0e31780c0f984
tatusmatrixAB <tatusmatrix@gmail.com>
2024-06-12 18:33:50 +0300

    Nodarbiba_07a

---
+++ Nodarbiba_07/JS/script.js
@@ -0,0 +1,11 @@
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  
+  result = myFunction(5, 6);
+  document.getElementById("demo1").innerHTML = result;
+  
