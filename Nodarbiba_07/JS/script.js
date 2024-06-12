129600ea9bf22c4fbf2574211aa368adc7f26daf
tatusmatrixAB <tatusmatrix@gmail.com>
2024-06-12 22:45:58 +0300

    Nodarbiba_07b

--- Nodarbiba_07/JS/script.js
+++ Nodarbiba_07/JS/script.js
@@ -1,11 +1,14 @@
-function myFunction(p1, p2) {
-    return p1 * p2;
-  }
-  
-  let result = myFunction(4, 3);
+  let result;
+
+  result = myFunction(4, 3);
   document.getElementById("demo").innerHTML = result;
   
   
   result = myFunction(5, 6);
   document.getElementById("demo1").innerHTML = result;
   
+
+  let text = "nsldnlsdmvldsfnvjnf.vmsdl;vsldk";
+  let symbolToFind = 'b';
+
+  console.log(indexOf(text, symbolToFind));
---
+++ Nodarbiba_07/JS/script2.js
@@ -0,0 +1,6 @@
+//https://chatgpt.com/share/feb1e51b-c1db-4198-a8af-e17f82ff2b26
+
+
+function random(a,b) {
+    return Math.floor(Math.random() * (b-a) + a);
+  }
